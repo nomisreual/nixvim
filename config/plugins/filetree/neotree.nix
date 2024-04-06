263d81439f26727d8c5b87cdd0438793a052ad4a
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-04-06 22:09:36 +0200

    feat: added image viewer

--- config/plugins/filetree/neotree.nix
+++ config/plugins/filetree/neotree.nix
@@ -1,3 +1,10 @@
+# Note: nvim-web-devicons is already enabled
+# Note: plenary-nvim is already enabled
 {
   plugins.neo-tree.enable = true;
+  # for previewing images
+  plugins.image = {
+    enable = true;
+    backend = "kitty";
+  };
 }
